55d52a4335b2119463c24c1e9c45acdc3f5b902e
David Santana <89950671+DvRobles@users.noreply.github.com>
2023-05-16 08:17:45 -0600

    Update users.js

--- users.js
+++ users.js
@@ -29,14 +29,8 @@ module.exports = {
     }
 
     next();
-  }
-};
-
-
-// middleware/users.js
-
-
-isLoggedIn: (req, res, next) => {
+  },
+  isLoggedIn: (req, res, next) => {
     try {
       const token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(
@@ -51,3 +45,9 @@ isLoggedIn: (req, res, next) => {
       });
     }
   }
+};
+
+
+// middleware/users.js
+
+
